d9aca203d99477c0c08c80ae8c03f2b996ee9f80
Maria Pol <maria.pujolpol@gmail.com>
2026-03-10 18:07:52 +0100

    css import MyListings

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -5,7 +5,7 @@ import ErrorMessage from "../components/ErrorMessage";
 import ListingCard from "../components/ListingCard";
 import { Link } from "react-router-dom";
 import { addFavorite } from "../services/favorites.service";
-import "../css/Mylistings.css";
+import "../css/MyListings.css";
 
 function Listings() {
   const [listings, setListings] = useState([]);
